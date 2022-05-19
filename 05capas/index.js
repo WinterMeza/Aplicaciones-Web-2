05c6d8098ba16db2696acc1a80a35b63a6f2222f
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-05-18 21:23:37 -0500

    Arquitectura de capas. Miércoles, 18/5/2022.

---
+++ 05capas/index.js
@@ -0,0 +1,9 @@
+// APLICACIONES WEB 2
+// Nombres y Apellidos: Winter Aníbal Meza Jiménez.
+// Curso: Sexto "B" 2022(1).
+// Clase: Arquitectura de capas.
+// Fecha: Martes, 17 de mayo de 2022.
+// Docente: Ing. Jhon Antonio Cevallos Macías, Mg.
+//Archivo de barril.
+// Se extrae la libreria dotenv y el archivo config.js de la base de datos.
+require('detenv').config()
