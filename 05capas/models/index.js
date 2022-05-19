e188e87fd9d0ea64f3591e72e36c15264eb868a6
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-05-18 21:13:45 -0500

    Arquitectura de capas. 18/05/22.

--- 05capas/models/index.js
+++ 05capas/models/index.js
@@ -2,11 +2,12 @@
 // Nombres y Apellidos: Winter Aníbal Meza Jiménez.
 // Curso: Sexto "B" 2022(1).
 // Clase: Arquitectura de capas.
-// Fecha: Lunes, 16 de mayo de 2022.
+// Fecha:Miércoles, 18 de mayo de 2022.
 // Docente: Ing. Jhon Antonio Cevallos Macías, Mg.
+// Se extrae de la capa de controllers.
 const Producto = require('./producto');
 
-
+// Se exporta el módulo Producto.
 module.exports = {
     Producto,
 }
