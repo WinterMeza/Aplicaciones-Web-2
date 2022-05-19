bb63031dfec2ddb33bec2f7255c908617b6032d1
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-05-18 21:17:20 -0500

    Delete index.js

--- 05capas/index.js
+++
@@ -1,6 +0,0 @@
-// APLICACIONES WEB 2
-// Nombres y Apellidos: Winter Aníbal Meza Jiménez.
-// Curso: Sexto "B" 2022(1).
-// Clase: Arquitectura de capas.
-// Fecha: Lunes, 16 de mayo de 2022.
-// Docente: Ing. Jhon Antonio Cevallos Macías, Mg.
