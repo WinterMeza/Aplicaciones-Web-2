d64a92cd7b4e46d2f936f91bdcb95e5191536973
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-05-21 21:58:05 -0500

    Configuración para llamar al servidor en el index.
     Fecha: Martes, 17 de mayo de 2022. Modificado: Sábado/5/2022,

--- 05capas/index.js
+++ 05capas/index.js
@@ -2,8 +2,12 @@
 // Nombres y Apellidos: Winter Aníbal Meza Jiménez.
 // Curso: Sexto "B" 2022(1).
 // Clase: Arquitectura de capas.
-// Fecha: Martes, 17 de mayo de 2022.
+// Fecha: Martes, 17 de mayo de 2022.Modificado: Sábado/5/2022.
 // Docente: Ing. Jhon Antonio Cevallos Macías, Mg.
 //Archivo de barril.
 // Se extrae la libreria dotenv y el archivo config.js de la base de datos.
-require('detenv').config()
+require('dotenv').config()
+    // Bloque de código que llama al servidor.
+const Server = require('./server');
+const server = new Server();
+server.listen();
