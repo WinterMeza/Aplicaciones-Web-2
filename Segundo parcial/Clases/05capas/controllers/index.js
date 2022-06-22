077eb8b1e888fab105bd4b5ac597f559e6a217b2
WinterMeza <e1317233748@live.uleam.edu.ec>
2022-06-21 21:59:47 -0500

    Ejemplo Docker 21/6/2022.

---
+++ Segundo parcial/Clases/05capas/controllers/index.js
@@ -0,0 +1,12 @@
+// APLICACIONES WEB 2
+// Nombres y Apellidos: Winter Aníbal Meza Jiménez.
+// Curso: Sexto "B" 2022(1).
+// Clase: Arquitectura de capas- Ejemplo de Docker.
+// Fecha: Miércoles, 18 de mayo de 2022. Modificado  21/6/2022.
+// Docente: Ing. Jhon Antonio Cevallos Macías, Mg.
+//Archivo de barril.
+const Producto = require('./producto')
+    // Se exporta el modulo.
+module.exports = {
+    Producto,
+}
