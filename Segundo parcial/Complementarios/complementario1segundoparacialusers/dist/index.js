d5a3758aec3a12e288378342acab332b740da66b
WinterMeza <e1317233748@live.uleam.edu.ec>
2022-06-26 18:14:06 -0500

    Delete por actualización 26/6/2022.

--- Segundo parcial/Complementarios/complementario1segundoparacialusers/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-const server_1 = require("./server");
-(0, dotenv_1.config)();
-const servidor = new server_1.Server();
-servidor.listen();
--- Segundo parcial/Complementarios/complementario1segundoparacialusers/index.ts
+++
@@ -1,6 +0,0 @@
-import { config } from "dotenv";
-import {Server} from './server';
-
-config();
-const servidor = new Server();
-servidor.listen();
