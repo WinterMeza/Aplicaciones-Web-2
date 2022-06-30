b21f2ffddf215079ef519756fc840765bd308b97
WinterMeza <e1317233748@live.uleam.edu.ec>
2022-06-30 00:00:09 -0500

    Avance CSR 29/6/2022.

--- Segundo parcial/Clases/05capas/routes/producto.js
+++ Segundo parcial/Clases/05capas/routes/producto.js
@@ -4,9 +4,11 @@
 // Clase: Arquitectura de capas- Ejemplo de Docker.
 // Fecha: Miércoles, 18 de mayo de 2022. Modificado  21/6/2022.
 // Docente: Ing. Jhon Antonio Cevallos Macías, Mg.
+
+
 // Se exportan las librerías que vamos a usar para las validaciones 
 //en este caso express para las rutas  para las rutas y express-validator para el check.
-const { Router } = require('express'); //  
+const { Router } = require('express');
 const { check } = require('express-validator');
 // Se agregan los controladores.
 const {
@@ -16,12 +18,14 @@ const {
     obtenerProducto,
     obtenerProductos
 } = require('../controllers').Producto;
+
+
 const { validarCampos } = require('../middlewares');
+
 const router = Router();
 //Validar que el Id exista en Mongo.
 router.get('/', obtenerProductos);
 router.get('/:id', check('id', 'El id no es válidio').isMongoId(), validarCampos, obtenerProducto);
-//Validar que no este vacío.
 router.post('/', check('nombre', 'El nombre es requerido').not().isEmpty(), validarCampos, crearProducto);
 router.put('/:id', check('id', 'No es un ID válido').isMongoId(), validarCampos, actualizarProducto);
 router.delete('/:id',
